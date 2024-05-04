6947549b4d7af40705ccf22e2daf6143822766f4
Beamlak996 <beamlaktsegaye996@gmail.com>
2024-05-04 09:21:44 -0900

    express setup done

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
+import "dotenv/config"
 import express from "express"
+import cors from "cors"
+import compression from "compression"
+
 
 const app = express()
+
+app.use(cors())
+app.use(compression())
+app.use(express.json())
 
 app.listen(3000, ()=> {
     console.log("Server listening to port 3000")
