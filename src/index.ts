2b5733632b4281e2cc301206bc45463c181b144b
Beamlak996 <beamlaktsegaye996@gmail.com>
2024-05-04 09:59:17 -0900

    handle errors done

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,10 @@ import "dotenv/config"
 import express from "express"
 import cors from "cors"
 import compression from "compression"
+import cookieParser from "cookie-parser"
+
 import connectDB from "./utils/db"
+import { ErrorMiddleware } from "./middleware/error"
 
 
 const app = express()
@@ -10,8 +13,20 @@ const app = express()
 app.use(cors())
 app.use(compression())
 app.use(express.json())
+app.use(cookieParser())
 
-app.listen(3000, ()=> {
-    console.log("Server listening to port 3000")
+
+// unknown routes
+app.all("*", (req: express.Request, res: express.Response, next: express.NextFunction)=> {
+    const err = new Error(`Route ${req.originalUrl} is not found`) as any
+    err.statusCode = 404
+    next(err)
+})
+
+
+app.use(ErrorMiddleware)
+
+app.listen(8000, ()=> {
+    console.log("Server listening to port 8000")
     connectDB()
 })
